e227e39f7166143e357aab6c8464c85f56cd161c
Karol Podsiadlo <karolekk131@gmail.com>
2023-01-26 17:32:27 +0100

    prepare to microservices app

--- README.md
+++
@@ -1,4 +0,0 @@
-### Bank Web Application
-###### #spring boot/mvc/security #thymeleaf #bootstrap #java #html/css #hiberante #mysql
-
-Application in progress
--- src/main/java/com/bankwebapp/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bankwebapp.repository;
-
-import com.bankwebapp.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-}
