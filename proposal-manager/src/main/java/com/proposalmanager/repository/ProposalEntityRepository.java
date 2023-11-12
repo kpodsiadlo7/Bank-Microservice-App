15d63feefc5bbe3ad9034fcc2a7aff4ba4a68828
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-12 17:54:11 +0100

    Proposal Manager Repository to package access

--- proposal-manager/src/main/java/com/proposalmanager/repository/ProposalEntityRepository.java
+++ proposal-manager/src/main/java/com/proposalmanager/repository/ProposalEntityRepository.java
@@ -1,11 +1,10 @@
 package com.proposalmanager.repository;
 
 import com.proposalmanager.domain.ProposalEntity;
-import com.proposalmanager.domain.enums.StatusProposal;
 import com.proposalmanager.repository.adapter.AdapterProposalEntityRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProposalEntityRepository extends AdapterProposalEntityRepository, JpaRepository<ProposalEntity,Long> {
+interface ProposalEntityRepository extends AdapterProposalEntityRepository, JpaRepository<ProposalEntity,Long> {
 }
