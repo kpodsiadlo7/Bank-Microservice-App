328290d9b30c310b8381b4a1fd28d4fa022e7dca
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-12 17:56:19 +0100

    User Manager Repository to package access

--- user-manager/src/main/java/com/usermanager/repository/UserEntityRepository.java
+++ user-manager/src/main/java/com/usermanager/repository/UserEntityRepository.java
@@ -1,12 +1,10 @@
 package com.usermanager.repository;
 
-import com.usermanager.repository.adapter.AdapterUserEntityRepository;
 import com.usermanager.domain.UserEntity;
+import com.usermanager.repository.adapter.AdapterUserEntityRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface UserEntityRepository extends AdapterUserEntityRepository, JpaRepository<UserEntity,Long> {
+interface UserEntityRepository extends AdapterUserEntityRepository, JpaRepository<UserEntity,Long> {
 }
