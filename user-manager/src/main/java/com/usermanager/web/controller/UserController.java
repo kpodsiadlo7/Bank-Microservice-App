85b1fe1bab180b79ca9811c822b09348aaf9c810
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-12 16:00:24 +0100

    User Controller -> Access package

--- user-manager/src/main/java/com/usermanager/web/controller/UserController.java
+++ user-manager/src/main/java/com/usermanager/web/controller/UserController.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-public class UserController {
+class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
     @GetMapping("/login")
-    public ResponseEntity<UserDto> loginUser(@RequestParam String username) {
+    ResponseEntity<UserDto> loginUser(@RequestParam String username) {
         return ResponseEntity.ok(userMapper.mapToUserDtoFromUser(userService.loginUser(username)));
     }
     @GetMapping("/check-user")
-    public ResponseEntity<Boolean> checkUserInDatabaseByUsername(@RequestParam String username){
+    ResponseEntity<Boolean> checkUserInDatabaseByUsername(@RequestParam String username){
         return ResponseEntity.ok(userService.checkIfUserExistInDbWithThisUsername(username));
     }
 
     @GetMapping("/get-user")
-    public ResponseEntity<UserDto> getUserById(@RequestParam Long userId){
+    ResponseEntity<UserDto> getUserById(@RequestParam Long userId){
         return ResponseEntity.ok(userMapper.mapToUserDtoFromUser(userService.getUserById(userId)));
     }
     @PostMapping("/create-user")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
         return ResponseEntity.ok(userMapper.mapToUserDtoFromUser(userService.validateData(userMapper.mapToUserFromUserDto(userDto))));
     }
 
