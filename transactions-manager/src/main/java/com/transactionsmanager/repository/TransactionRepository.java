7b2f5567dca071d92be7c0f5018d9b9d98ccef21
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2023-11-12 17:55:45 +0100

    Transaction Manager Repository to package access

--- transactions-manager/src/main/java/com/transactionsmanager/repository/TransactionRepository.java
+++ transactions-manager/src/main/java/com/transactionsmanager/repository/TransactionRepository.java
@@ -4,8 +4,6 @@ import com.transactionsmanager.domain.TransactionEntity;
 import com.transactionsmanager.repository.adapter.AdapterTransactionRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface TransactionRepository extends AdapterTransactionRepository, JpaRepository<TransactionEntity, Long> {
+interface TransactionRepository extends AdapterTransactionRepository, JpaRepository<TransactionEntity, Long> {
 
 }
